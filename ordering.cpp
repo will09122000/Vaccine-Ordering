4cee144505dc2d709d5568bba89252f678bcae42
Will <will0912@icloud.com>
2021-02-26 12:06:44 +0000

    Update ordering.cpp

--- ordering.cpp
+++ ordering.cpp
@@ -26,6 +26,7 @@ int main(int argc, char **argv) {
                 endDay(line, customers, orders, invoiceNumber);
                 break;
         }
+        cout << "-------------------------------------------------------------------------\n";
     }
     return 0;
 }
