46e1c4fa07d9bd4f4e05eaa496a5bafb4ad25c9c
Will <will0912@icloud.com>
2021-02-28 16:08:25 +0000

    Update customer.cpp

--- customer.cpp
+++ customer.cpp
@@ -17,7 +17,7 @@ class Customer
         void printInvoice()
         {
             //Invoice invoice = this->invoices.back();
-            //cout << invoice.id << "\n";
+            cout << this->id << "\n";
         }
 
 
