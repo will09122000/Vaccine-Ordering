fd75a8873a5b4eed458ee46e5bd850c919168557
Will <will0912@icloud.com>
2021-02-28 15:37:10 +0000

    Update ordering.hpp

--- ordering.hpp
+++ ordering.hpp
@@ -1,5 +1,6 @@
 #include <fileIO.cpp>
 #include <order.cpp>
+#include <invoice.cpp>
 
 void addCustomer(string line, vector<Customer> & customers);
 void addOrder(string line, vector<Order> & orders, vector<Customer> & customers, int & invoiceNumber);
