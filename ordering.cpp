ca53cde7b6522661775cf0c26b9f921728b6d915
Will <will0912@icloud.com>
2021-02-28 16:24:20 +0000

    Update ordering.cpp

--- ordering.cpp
+++ ordering.cpp
@@ -86,7 +86,7 @@ void sendOrder(int customerID, vector<Order> & orders, int & invoiceNumber, vect
     {
         if (customer.id == customerID)
         {
-            Invoice invoice = customer.invoices.back();
+            Invoice invoice = customer->invoices.back();
         }
     }
 
