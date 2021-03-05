9164b34c64071a751574c02ae126eb11608e0b06
Will <will0912@icloud.com>
2021-03-05 13:07:19 +0000

    y

--- fileIO.cpp
+++ fileIO.cpp
@@ -21,7 +21,7 @@ bool isValidLine(string line)
     if (line.length() > 45)
         return false;
 
-    // Check first character
+    // Check first character and its following parameters
     switch(line.front()) 
     {
         // New customer
@@ -35,11 +35,22 @@ bool isValidLine(string line)
         // New order
         case 'S':
         {
+            string date = line.substr(1, 8);
+            char type = line.substr(9, 1);
+            string customerID = line.substr(10, 4);
+            string orderQuantity = line.substr(14, 3);
+            return (!date.empty() && std::all_of(date.begin(), date.end(), ::isdigit)) ||
+                   (type == 'N' || type == 'X') ||
+                   (!customerID.empty() && std::all_of(customerID.begin(), customerID.end(), ::isdigit)) ||
+                   (!orderQuantity.empty() && std::all_of(orderQuantity.begin(), orderQuantity.end(), ::isdigit));
             break;
         }
         // End-of-day
         case 'E':
         {
+            string date = line.substr(1, 8);
+            return !date.empty() &&
+                    std::all_of(date.begin(), date.end(), ::isdigit);
             break;
         }
         default:
